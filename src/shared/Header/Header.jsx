a1b0befee37a646a6c4e7ba4adc873519d51c7ae
Abdur-P-Rahim <bspirahim@gmail.com>
2023-05-04 16:32:35 +0600

    banner added

--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -1,28 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
     return (
         <div>
-            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="#home">Food-Hub</Navbar.Brand>
+                    <Navbar.Brand href="#home" className='fs-4'>Food-Hub</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="m-auto">
-                            
-                        <Link className='me-3 text-decoration-none text-black fs-5' to="/">Home</Link>
-                            <Link className='me-3 text-decoration-none text-black fs-5' to="blog">Blog</Link>
-                            <Link className='me-3 text-decoration-none text-black fs-5' to="about">About Us</Link>
-                            
+
+                            <Link className='me-3 text-decoration-none text-light fs-5' to="/">Home</Link>
+                            <Link className='me-3 text-decoration-none text-light fs-5' to="blog">Blog</Link>
+                            <Link className='me-3 text-decoration-none text-light fs-5' to="about">About Us</Link>
+
                         </Nav>
                         <Nav>
-                        <Link className='me-3 text-decoration-none text-black fs-5' to="login">Login</Link><Link className='me-3 text-decoration-none text-black fs-5' to="register">Register</Link>
+                            <Link className='me-3 text-decoration-none text-light fs-5' to="login">Login</Link><Link className='me-3 text-decoration-none text-light fs-5' to="register">Register</Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
+            <div className='position-relative text-center'>
+                <img className='banner w-100' src="../../../public/images/cover/cover-2.jpg" class="img-fluid" alt="Responsive image" />
+                <div className='text-light position-absolute bottom-50 end-50'>
+                    <p className='fs-1 fw-bold '>Nature Has Always <br /> Care For Us</p>
+                    <Button variant="light">Get More</Button>
+                </div>
+                
+            </div>
         </div>
     );
 };
